0a744596206088f5dcf387a62a8eb6ed6e54212c
rishav-karanjit <karanjitrishav4@gmail.com>
2025-01-17 22:43:22 -0800

    Add something on extern

---
+++ DynamoDbEncryption/runtimes/go/ImplementationFromDafny-go/InternalLegacyOverride/extern.go
@@ -0,0 +1,17 @@
+package InternalLegacyOverride
+
+import (
+	"github.com/aws/aws-cryptographic-material-providers-library/releases/go/smithy-dafny-standard-library/Wrappers"
+	"github.com/aws/aws-database-encryption-sdk-dynamodb/awscryptographydbencryptionsdkdynamodbitemencryptorsmithygeneratedtypes"
+)
+
+// var Companion_InternalLegacyOverride_ = CompanionStruct_Default___{}
+
+func (CompanionStruct_InternalLegacyOverride_) Build(config interface{}) Wrappers.Result {
+	// Create error for unsupported legacy configuration
+	err := &awscryptographydbencryptionsdkdynamodbitemencryptorsmithygeneratedtypes.DynamoDbItemEncryptorException{
+		Message: "Legacy configuration unsupported.",
+	}
+	// Create and return a failure result
+	return Wrappers.Companion_Result_.Create_Failure_(err)
+}
